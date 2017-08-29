9200bdb50c9c792a6ffaf3f5b87e0ce66e14db1f
qiang.li <qiang.li@timanetworks.com>
2017-08-29 16:56:10 +0800

    add chacha20

--- shadowstream/cipher.go
+++ shadowstream/cipher.go
@@ -89,3 +89,22 @@ func Xchacha20(key []byte) (Cipher, error) {
 	}
 	return xchacha20key(key), nil
 }
+
+type chacha20key []byte
+
+func (k chacha20key) IVSize() int                       { return chacha20.NonceSize }
+func (k chacha20key) Decrypter(iv []byte) cipher.Stream { return k.Encrypter(iv) }
+func (k chacha20key) Encrypter(iv []byte) cipher.Stream {
+	ciph, err := chacha20.NewCipher(k, iv)
+	if err != nil {
+		panic(err) // should never happen
+	}
+	return ciph
+}
+
+func Chacha20(key []byte) (Cipher, error) {
+	if len(key) != chacha20.KeySize {
+		return nil, KeySizeError(chacha20.KeySize)
+	}
+	return chacha20key(key), nil
+}
